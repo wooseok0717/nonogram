ba5d29449d04118f3e20476a082d31b426e019e9
Josh <wjang717@gmail.com>
2023-02-27 17:10:31 -0800

    initialize repo

---
+++ server/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+// const router = require('./routes');
+
+const app = express();
+const { PORT } = process.env;
+
+app.use(morgan('dev'));
+app.use(express.json());
+// app.use('/api', router);
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.listen(PORT, () => {
+  console.log(`Server Listening at http://localhost:${PORT}`);
+});
